37332e8ac1e71ebab966d418370b8e82dd7d791c
Rutwij Vaykode <rutwijrick@gmail.com>
2025-08-08 15:05:33 +0530

    Update DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,16 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, 
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
-    timezone: '+05:30', 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    timezone: '+05:30',
 });
 
 export default sequelize;
