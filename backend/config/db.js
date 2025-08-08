78e16cc9fbaf2975f0435517c3009ba5a0e3d2a2
Rutwij Vaykode <rutwijrick@gmail.com>
2025-08-08 15:59:06 +0530

    New db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,20 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            ca: process.env.DB_CA_CERT?.replace(/\\n/g, '\n'),
-        },
-    },
-    timezone: '+05:30',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'mysql',
+        logging: false,
+        timezone: '+05:30',
+        dialectOptions: {
+            ssl: false // AlwaysData does not require SSL by default
+        }
+    }
+);
 
 export default sequelize;
